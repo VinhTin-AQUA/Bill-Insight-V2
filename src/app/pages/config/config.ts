3b9509d987834a8f944190c2f045820d67e573d8
VinhTin-AQUA <tinhovinh@gmail.com>
2025-10-26 23:25:59 +0700

    update

--- src/app/pages/config/config.ts
+++ src/app/pages/config/config.ts
@@ -25,7 +25,7 @@ export class Config {
     async test() {
         const r = await this.tauriCommandSerivce.invokeCommand<string>(
             TauriCommandSerivce.INIT_GOOGLE_SHEET_COMMAND,
-            { jsongPath: 'C:/Users/tinhv/Downloads/billinsight-0b2c14cec552.json' }
+            { jsonPath: 'C:/Users/tinhv/Downloads/billinsight-0b2c14cec552.json' }
         );
         alert(r);
     }
